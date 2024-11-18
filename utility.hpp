1b4e2c492e30e71cd0b57df4d9a59ae7b597c88d
Hugo Santos <124203466+Hugosntss@users.noreply.github.com>
2024-11-17 19:11:31 -0800

    utilities added

---
+++ utility.hpp
@@ -0,0 +1,10 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <vector>
+#include "transaction.hpp"
+
+void generateSummary(const std::vector<Transaction>& transactions);
+void displayMenu();
+
+#endif 
