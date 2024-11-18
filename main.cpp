7a152a85698591be627c95819be89f06a949f022
Hugo Santos <124203466+Hugosntss@users.noreply.github.com>
2024-11-17 19:02:22 -0800

    main expensive tracker
    expensive tracker begins

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "transaction.hpp"
+#include "utility.hpp"
+
+int main() {
+    std::vector<Transaction> transactions;
+    std::string filename = "transactions.txt";
+    int choice;
+
+    do {
+        displayMenu();
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addTransaction(transactions);
+                break;
+            case 2:
+                viewTransactions(transactions);
+                break;
+            case 3:
+                saveTransactions(transactions, filename);
+                break;
+   
+            case 6:
+                std::cout << "Goodbye!\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 6);
+
+    return 0;
+}
